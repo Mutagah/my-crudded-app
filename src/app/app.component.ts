31e86d52fe362311b16c56836fec71507787e1d4
Mutagah <kihahumutagah@gmail.com>
2024-03-06 14:29:35 +0300

    :lipstick:feat:UI change.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataFetchService } from './data-fetch.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private dataFetch: DataFetchService) {
+    console.log(this.dataFetch.getProducts());
+  }
   title = 'my-cruded-app';
+  products!: any;
+  ngOnInit(): void {
+    this.dataFetch.getProducts().subscribe({
+      // When we get the values successfully
+      next: (res: any) => {
+        // response is returned on a successful response
+        return this.products = res;
+      },
+      // When we encounter an error
+      error: (error: any) => {
+        console.log('The error block');
+        console.log(error);
+      },
+      //      Runs after the data being fetched has completed
+      complete: () => {
+        console.log('The completion block');
+      },
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// Importing the http Client Module so as to use HttpClient in our service
+import { HttpClientModule } from '@angular/common/http';
+
+import { DataFetchService } from './data-fetch.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [DataFetchService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
