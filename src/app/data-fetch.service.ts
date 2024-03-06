970b1a2ae36df24a461e2a91ea6b1e4392f17e5e
Mutagah <kihahumutagah@gmail.com>
2024-03-06 14:24:34 +0300

    :sparkles:feat:Creation of data-fetch service.

---
+++ src/app/data-fetch.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root',
+})
+export class DataFetchService {
+  url = 'http://localhost:3000/products';
+  constructor(private http: HttpClient) {}
+   getProducts(): Observable<any> {
+    return this.http.get(this.url);
+  }
+}
