226e1ecc141ebced81ddfb9f69685e489f521fa0
Mutagah <kihahumutagah@gmail.com>
2024-03-07 09:28:25 +0300

    :necktie:feat:Addition of the post function.

--- src/app/data-fetch.service.ts
+++ src/app/data-fetch.service.ts
@@ -7,7 +7,13 @@ import { Observable } from 'rxjs';
 export class DataFetchService {
   url = 'http://localhost:3000/products';
   constructor(private http: HttpClient) {}
-   getProducts(): Observable<any> {
+  getProducts(): Observable<any> {
     return this.http.get(this.url);
+  }
+  /*
+   Method to post my data that take a parameter called data of type any
+   */
+  postData(data: object): Observable<any> {
+    return this.http.post<any>(`${this.url}`, data);
   }
 }
