13a40aa14c3a6ba7d7ae2861c4352d67d1af3f4e
Mutagah <kihahumutagah@gmail.com>
2024-03-07 09:24:14 +0300

    :lipstick::necktie:feat:Creation of reactive form UI and handling of its logic.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { HomeComponent } from './home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, ReactiveFormComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
   providers: [DataFetchService],
   bootstrap: [AppComponent],
 })
